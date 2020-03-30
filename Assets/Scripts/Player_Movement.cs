7780a793fdc8606d531cdbd0ea31410a81817038
Strudenjaden <48246921+Strudenjaden@users.noreply.github.com>
2020-03-30 18:58:27 +0200

    Initial commit

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed = 5;
+    public float jumpForce = 10;
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        //Inputs
+        movement.x = Input.GetAxisRaw("Horizontal");
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * 10;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MiniGame01_2D
+ First try to making a game on 2D. This project is used to lear about 2D coding.
