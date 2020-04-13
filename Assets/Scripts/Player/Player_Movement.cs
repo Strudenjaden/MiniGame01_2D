651d085524d4b5a530e582ec0db19caabd5903f2
Strudenjaden <48246921+Strudenjaden@users.noreply.github.com>
2020-04-13 21:40:56 +0200

    Fix for ladder and hitboxes. Controller Support implemented.
    Fix for ladder and hitboxes.
    Changes on the script to climb ladders.
    -You can jump now if u are on ladder but not climbing.
    -You can't jump on ladders.
    -If you hold the jump button on ladder the player will stay on the position. Dont fell off.
    -If you dont climb the player will fell off.
    
    The hitbox have been fixed.
    Now you bounce when jump from a High place.
    
    Xbox Controller support :D

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,29 +15,25 @@ public class Player_Movement : MonoBehaviour
     [Tooltip("Player's speed")]
     public float speed = 25f;
     [Tooltip("Player's speed when is climbing the ladder")]
-    public float speedOnLadder = 10f;
+    public float speedOnLadder = 5f;
     [Tooltip("The movement of the player. Input * speed")]
     public float movement;
 
     [Header("Player boolean actions")]
     [Tooltip("Is the player jumping?")]
     public bool jump;
-    [Tooltip("Is the player climbing?")]
-    public bool climbing;
-    [Tooltip("Is the player on the ladder?")]
-    public bool onLadder;
+    [Tooltip("Is the player climbing up?")]
+    public bool climbingUp;
 
     [Header("Ladder Raycast")]
     public float distance;
     public LayerMask whatIsLadder;
 
-
-    
-
     void Start()
     {
-        //At the start we save the first value for gravity and dont change it wrongly.
+        //At the start we save the first value for gravity and dont change it wrongly in the future.
         firstGravity = rb.gravityScale;
+
     }
 
     // Update is called once per frame
@@ -46,11 +43,12 @@ public class Player_Movement : MonoBehaviour
         movement = Input.GetAxisRaw("Horizontal") * speed;
 
         //Making player jump when isn't on a Ladder
-        if (Input.GetKeyDown(KeyCode.Space) && !climbing && !onLadder)
+        if (Input.GetButtonDown("Jump"))
         {
             jump = true;
         }
 
+        Debug.Log(rb.gravityScale);
     }
 
     void FixedUpdate()
@@ -60,7 +58,7 @@ public class Player_Movement : MonoBehaviour
 
         //Climb ladders
         Climbing();
-        
+
     }
 
     public void Movement()
@@ -73,44 +71,49 @@ public class Player_Movement : MonoBehaviour
 
     public void Climbing()
     {
-        
+         bool climbing; //Is the player climbing?
+
         RaycastHit2D hitLadder = Physics2D.Raycast(transform.position, Vector2.down, distance, whatIsLadder); //What is +
 
+        //Collides with a ladder
         if (hitLadder.collider != null)
         {
-            onLadder = true;
             Debug.DrawRay(transform.position, Vector2.down * distance, Color.green);
             Debug.Log("I'm on the ladder");
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetAxisRaw("Vertical") > 0) //If the user want to go up.
             {
-
+                climbingUp = true;
                 climbing = true;
             }
             else
             {
-                //If is on the ladder the player keep his position (don't go down) and lowers his speed.
-                rb.velocity = new Vector2(rb.velocity.x / 2, 0.981f); //0.981f works for me. The player stays on the same position but gravity keep forcing to go down the player.
                 climbing = false;
-                //rb.gravityScale = firstGravity;
-            }
+                climbingUp = false;
+            } 
+
         }
         else
         {
             Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
             Debug.Log("I'm not on ladder");
-            climbing = false;
-            onLadder = false;
+            //Reset the climbing booleans when hit the top of the ladder or when the player isn't in the ladder.
+            climbing = false; 
+            climbingUp = false;
         }
 
-        if (climbing == true)
+        //The velocity on X and Y when the player ir climbing.
+        if (climbingUp == true)
         {
-            rb.velocity = new Vector2(rb.velocity.x / 2, 3f);
-            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x / 2, speedOnLadder);
+
+            //If the player "jumps" when its climbing the player will stay in the air.
+            if (Input.GetButton("Jump"))
+            {
+                rb.velocity = new Vector2(rb.velocity.x / 2, 0.981f);
+            }
+            
         }
-        else
-        {
-            rb.gravityScale = firstGravity;
-        }
+
     }
 
     /*public void OnTriggerStay2D(Collider2D other)
@@ -122,26 +125,20 @@ public class Player_Movement : MonoBehaviour
             speedOnLadder = rb.velocity.x / 2;
             if (Input.GetKey(KeyCode.W))
             {
-
                 //Sube a 3f la escalera y sale a su velocidad normal
                 rb.velocity = new Vector2(0f, 3f); //El movimiento del personaje , velocidad de subida de escaleras
                 onLadder = true;
-
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 rb.velocity = new Vector2(0f, -3f);
                 onLadder = true;
-
             }else
             {
-
-
                 //Si ha subido y se mantiene, se realentiza su velocidad.
                 rb.velocity = new Vector2(rb.velocity.x, 0.981f); //0.981f works for me. The player stays on the same position.
                 
             }
-
         }
         
     }
